25fa2cb4c16fd88a30562f374e818d681cfb027f
Roman-Nechaev <nrchaevroman@gmail.com>
2022-02-04 22:43:29 +0200

    task-1-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const refs = {
+  bntStart: document.querySelector('button[data-start]'),
+  bntStop: document.querySelector('button[data-stop]'),
+  bodyEl: document.body,
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const DELAY = 1000;
+refs.bntStart.addEventListener('click', () => {
+  interval.start();
+});
+refs.bntStop.addEventListener('click', () => {
+  interval.stop();
+});
+
+//   ==========================================
+
+const interval = {
+  timerId: null,
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.timerId = setInterval(() => {
+      this.isActive = true;
+      const color = getRandomHexColor();
+      refs.bodyEl.style.backgroundColor = color;
+    }, DELAY);
+  },
+  stop() {
+    clearInterval(this.timerId);
+    this.isActive = false;
+  },
+};
